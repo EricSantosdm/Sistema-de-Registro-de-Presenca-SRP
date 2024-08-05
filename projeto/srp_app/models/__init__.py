52a4d4e54e7a434a9181bbec8bb8173bc0677560
flavionogueiraa <flavio.nogueira.profissional@gmail.com>
2024-08-05 20:04:30 -0300

    Model Evento

--- projeto/srp_app/models/__init__.py
+++ projeto/srp_app/models/__init__.py
@@ -1,1 +1,5 @@
-__all__ = []
+from .evento import Evento
+
+__all__ = [
+    Evento,
+]
---
+++ projeto/srp_app/models/evento.py
@@ -0,0 +1,28 @@
+from django.db import models
+from novadata_utils.models import AbstractNovadataModel
+
+
+class Evento(AbstractNovadataModel):
+    nome = models.CharField(
+        verbose_name="Nome",
+        max_length=100,
+    )
+
+    descricao = models.TextField(
+        verbose_name="Descrição",
+    )
+
+    data_inicial = models.DateTimeField(
+        verbose_name="Data inicial",
+    )
+
+    def __str__(self):
+        """Método que retorna a representação do objeto como string."""
+        return str(self.nome)
+
+    class Meta:
+        """Sub classe para definir meta atributos da classe principal."""
+
+        app_label = "srp_app"
+        verbose_name = "Evento"
+        verbose_name_plural = "Eventos"
