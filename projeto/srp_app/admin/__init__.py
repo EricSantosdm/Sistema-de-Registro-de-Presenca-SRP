821899236511fe6b6735f427b12111d404d52c6c
flavionogueiraa <flavio.nogueira.profissional@gmail.com>
2024-08-24 09:10:09 -0300

    feat: VisitanteAdmin

--- projeto/srp_app/admin/__init__.py
+++ projeto/srp_app/admin/__init__.py
@@ -1,7 +1,9 @@
 from .evento_admin import EventoAdmin
 from .participante_evento_inline import ParticipanteEventoInline
+from .visitante_admin import VisitanteAdmin
 
 __all__ = [
     EventoAdmin,
     ParticipanteEventoInline,
+    VisitanteAdmin,
 ]
