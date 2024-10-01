84377193f8a09faa6ce9a0284fc3dbff520dec9e
flavionogueiraa <flavio.nogueira.profissional@gmail.com>
2024-10-01 13:15:51 -0300

    feat: verbose_name da app

--- projeto/srp_app/apps.py
+++ projeto/srp_app/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class SrpAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "srp_app"
+    verbose_name = "Sistema de registro de presença"
 
     def ready(self):
         import srp_app.signals
