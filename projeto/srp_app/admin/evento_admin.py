c82f394e00a694041c5ee98b1f0e728209e59d7a
flavionogueiraa <flavio.nogueira.profissional@gmail.com>
2024-10-03 18:25:29 -0300

    feat: Custom page

--- projeto/srp_app/admin/evento_admin.py
+++ projeto/srp_app/admin/evento_admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
+from django.urls import path
+from django.views.generic import DetailView
 from novadata_utils.admin import NovadataModelAdmin
 from novadata_utils.redirect import reverse_lazy_plus
 from unfold.admin import ModelAdminMixin
 from unfold.decorators import action
+from unfold.views import UnfoldModelAdminViewMixin
 
 from ..models import Evento
 from .participante_evento_inline import ParticipanteEventoInline
+
+
+class InscreverseEventoView(UnfoldModelAdminViewMixin, DetailView):
+    title = "Increver-se no evento"
+
+    permission_required = ()
+
+    template_name = "home/evento/inscreverse.html"
+
+    model = Evento
 
 
 @admin.register(Evento)
@@ -18,6 +31,8 @@ class EventoAdmin(ModelAdminMixin, NovadataModelAdmin):
         "gerar_qrcode_inscricao",
         "gerar_qrcode_presenca",
     ]
+
+    # list_display = ["detail"]
 
     @action(attrs={"target": "_blank"})
     def gerar_qrcode_inscricao(self, request, obj):
@@ -39,4 +54,25 @@ class EventoAdmin(ModelAdminMixin, NovadataModelAdmin):
             get_params={
                 "acao": "marcar_presenca",
             },
+        )
+
+    def get_urls(self):
+        return [
+            path(
+                "<int:pk>/inscreverse/",
+                self.admin_site.admin_view(
+                    InscreverseEventoView.as_view(
+                        model_admin=self,
+                    )
+                ),
+                name="srp_app_evento_inscreverse",
+            ),
+            *super().get_urls(),
+        ]
+
+    def detail(self, obj: Evento) -> str:
+        return reverse_lazy_plus(
+            "admin:srp_app_evento_inscreverse",
+            url_params=[obj.pk],
+            just_uri=True,
         )
