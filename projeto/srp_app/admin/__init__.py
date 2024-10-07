afd021ab9c87a9169224af23538e5184af2842c6
flavionogueiraa <flavio.nogueira.profissional@gmail.com>
2024-10-07 19:35:03 -0300

    feat: Exportar presenças

--- projeto/srp_app/admin/__init__.py
+++ projeto/srp_app/admin/__init__.py
@@ -1,9 +1,11 @@
 from .evento_admin import EventoAdmin
+from .participante_evento_admin import ParticipanteEventoAdmin
 from .participante_evento_inline import ParticipanteEventoInline
 from .visitante_admin import VisitanteAdmin
 
 __all__ = [
     EventoAdmin,
+    ParticipanteEventoAdmin,
     ParticipanteEventoInline,
     VisitanteAdmin,
 ]
