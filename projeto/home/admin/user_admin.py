bffc9aefbe165044b01307473293d048f1841a24
flavionogueiraa <flavio.nogueira.profissional@gmail.com>
2024-10-13 15:12:52 -0300

    fix: ModelAdmin no lugar de ModelAdminMixin

--- projeto/home/admin/user_admin.py
+++ projeto/home/admin/user_admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from import_export.admin import ImportExportModelAdmin
-from unfold.admin import ModelAdminMixin
+from unfold.admin import ModelAdmin
 
 # from .profile_inline import ProfileInline
 
@@ -10,6 +10,6 @@ admin.site.unregister(User)
 
 
 @admin.register(User)
-class CustomUserAdmin(ModelAdminMixin, ImportExportModelAdmin, UserAdmin):
+class CustomUserAdmin(ModelAdmin, ImportExportModelAdmin, UserAdmin):
     pass
     # inlines = [ProfileInline]
